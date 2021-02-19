b2b2923ea3d7eadf3d4834d03313d39b10e29ef0
Lakshmi Srinivas <nivass2k@gmail.com>
2021-02-19 19:24:00 +0530

    removal of deprecated request

--- app.js
+++ app.js
@@ -1,49 +1,41 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const https=require("https");
-const got=require("got");
-
-const app=express();
-
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('superagent');
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html");
 });
 
-app.post("/",function(req,res){
-    var firstname=req.body.firstname;
-    var lastname=req.body.lastname;
-    var mail=req.body.inputEmail;
-    var data ={
-        members:[
-            {
-                email_address:mail,
-                status:"subscribed",
-                merge_fields:{
-                    FNAME:firstname,
-                    LNAME:lastname,
-                }
-            }
-        ]
-    };
-    const jsonData=JSON.stringify(data);
-    const url="https://us1.api.mailchimp.com/3.0/lists/5d35073faf";
-    const options={
-        method:"POST",
-        auth:"lucky:e0d9767ab5ddb47afb61e608c5d88548-us1"
-    }
-    const request=https.got(url,options,function(response){
-        response.on("data",function(data){
-            console.log(JSON.parse(data));
-        })
-    })
-    request.write(jsonData);
-    request.end();
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
-app.listen(process.env.PORT || 3000);
+var mailchimpInstance   = 'us1',
+    listUniqueId        = '5d35073faf',
+    mailchimpApiKey     = 'e0d9767ab5ddb47afb61e608c5d88548-us1';
 
-// 5d35073faf
-// e0d9767ab5ddb47afb61e608c5d88548-us1
+app.post('/', function (req, res) {
+    request
+        .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+        .set('Content-Type', 'application/json;charset=utf-8')
+        .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+        .send({
+          'email_address': req.body.inputEmail,
+          'status': 'subscribed',
+          'merge_fields': {
+            'FNAME': req.body.firstname,
+            'LNAME': req.body.lastname
+          }
+        })
+            .end(function(err, response) {
+              if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+                res.sendFile(__dirname+"/success.html");
+              } else {
+                res.send(__dirname+"/failure.html");
+              }
+          });
+});
